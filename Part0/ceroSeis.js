4a4e181babc9334679265b514ff836de85d9dac2
Gabriel Rotger <rotgergabriel@gmail.com>
2022-05-26 16:56:20 +0200

    Part0-Second SPA diagram

---
+++ Part0/ceroSeis.js
@@ -0,0 +1,11 @@
+// title Nueva nota usuario SPA
+
+// Usuario->Navegador: El usuario ingresa una nueva nota
+// Navegador->Servidor: Realiza una peticion a https://studies.cs.helsinki.fi/exampleapp/SPA
+// Servidor->Navegador: [{ Responde data.JSON}, ...] Actualizado
+
+// note over Usuario:
+// Se envia los datos de la nueva nota al servidor, 
+// luego el navegador reliza una peticion al servidor 
+// y el servidor responde solo con los datos JSON actualizados
+// end note
