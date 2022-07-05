785b3df6d69b976e7e6d04103af00c091c135443
Gabriel Rotger <rotgergabriel@gmail.com>
2022-07-05 12:28:12 +0200

    1.9: unicafe step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -22,12 +22,39 @@ const Display = ({props, name}) => {
    )
 }
 
-const Statistics = ({props, name, operator}) => {
+const Statistics = ({
+    allClicks, 
+    all, 
+    average, 
+    positive, 
+    allName, 
+    averageName, 
+    positiveName, 
+    operator, 
+    good, 
+    neutral, 
+    bad
+  }) => {
+
+    const textOne = 'No feedback given'
+
+    if((allClicks.length === 0)) {
+      return (
+        <div>
+            {textOne}
+        </div>
+      )
+    }
      return(
         <>
-          <p>{name} {props} {operator}</p>
+          <Display props = {good} name='Good: '/>
+          <Display props = {neutral} name='Neutral: '/>
+          <Display props = {bad} name='Bad: '/>
+          <p>{allName} {all} </p>
+          <p>{averageName} {average} </p>
+          <p>{positiveName} {positive} {operator}</p>
         </>
-     ) 
+     )
 }
 
 
@@ -35,25 +62,30 @@ const App =() => {
 
   const TitleProps = {
     title: 'Give feedback', 
-    subTitle:'Statistics'
+    secondTitle:'Statistics'
   }
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleGoodClicks = () => {
+    setAll(allClicks.concat(good))
     setGood(good + 1)
   }
 
   const handleNeutralClicks = () => {
+    setAll(allClicks.concat(neutral))
     setNeutral(neutral + 1)
   }
 
   const handleBadClicks = () => {
+    setAll(allClicks.concat(bad))
     setBad(bad + 1)
   }
 
+  //I could have used all instead of defining a new allClicks state, but it seemed more correct this way
   const all = good + neutral + bad
   const average = ((good - bad) / 3) / 3
   const positive = (good * 100) / all
@@ -64,16 +96,20 @@ const App =() => {
         <Buttons onClick={handleGoodClicks} text='Good' />
         <Buttons onClick={handleNeutralClicks} text='Neutral'/>
         <Buttons onClick={handleBadClicks} text='Bad'/>
-        <Title props={TitleProps.subTitle}/>
-        <Display props = {good} name='Good: '/>
-        <Display props = {neutral} name='Neutral: '/>
-        <Display props = {bad} name='Bad: '/>
-        <Statistics props= {all} name='All: '/>
-        <Statistics props= {average} name='Average: '/>
-        <Statistics 
-            props= {positive} 
-            name='Positive: ' 
-            operator={'%'} />
+        <Title props={TitleProps.secondTitle}/>
+        <Statistics
+            allClicks={allClicks} 
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            all= {all}
+            average= {average} 
+            positive={positive}
+            allName='All: '
+            averageName='Average: '
+            positiveName='Positive: '
+            operator={'%'}
+            />
       </>
   );
 }
