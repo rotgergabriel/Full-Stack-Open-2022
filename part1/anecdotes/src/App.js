179af267c2e1b0b4d84bcfa43e40437c92539c81
Gabriel Rotger <rotgergabriel@gmail.com>
2022-07-08 21:36:17 +0200

    1,14*: anecdotes, step 3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
+const Header = ({text}) => {
+  return(
+      <h1> {text} </h1>
+  )
+}
+
 const Button = ({handleClicks, text}) => {
     return(
         <button onClick={handleClicks}>{text}</button>
@@ -14,17 +20,30 @@ const Points = ({points, text}) => {
     )
 }
 
-const Anecdotes = ({anecdotes}) => {
+const Anecdotes = ({anecdotes, points, textNo, textHas, textVotes}) => {
     return(
-        <p>
-            {anecdotes}
-        </p>
+        <>
+            <p>{anecdotes}</p>
+            <p> {textNo} {textHas} {points} {textVotes} </p>
+        </>
     )
 }
+
+const Winner = ({anecdotes, points, text}) => {
+  if(points.reduce((a, b)=> a + b, 0) > 0) {
+    const result = points.indexOf(Math.max(...points))
+    return <Anecdotes textHas={text.name2} textVotes={text.name1} anecdotes={anecdotes[result]} points={points[result]} />
+  }else {
+    return <Anecdotes textNo={text.name3} />
+  }
+}
+
+
   
 const App = ({anecdotes}) => {
     const [selected, setSelected] = useState(0)
     const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+    
     const handleAnecdote = () => {
       setSelected(Math.floor(Math.random() * anecdotes.length))
     }
@@ -37,15 +56,22 @@ const App = ({anecdotes}) => {
 
     const text = {
       name: 'Next anecdote',
-      name1: 'Votes'
+      name1: 'Votes',
+      name2: 'Has',
+      name3: 'No existing votes',
+      name4: 'Anecdote of the day',
+      name5: 'Anecdote with most votes'
     }
 
     return (
         <div>
+          <Header text={text.name4} />
           <Button text={text.name} handleClicks={handleAnecdote} />
-          <Anecdotes text={text} anecdotes={anecdotes[selected]} />
-          <Button text={text.name1} handleClicks={handlePoints} />
+          <Anecdotes anecdotes={anecdotes[selected]} />
           <Points text={text.name1} points={points[selected]} />
+          <Button text={text.name1} handleClicks={handlePoints} />
+          <Header text={text.name5} />
+          <Winner text={text} anecdotes={anecdotes} points={points} />
         </div>
     );
 }
